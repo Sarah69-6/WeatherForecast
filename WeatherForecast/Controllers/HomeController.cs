09645c2e586253d855465ab50aa71fcab6e1b7b5
Sarah Skeels <Sarah.Skeels@cws-uk.com>
2020-07-14 10:18:39 +0100

    Add project files.

---
+++ WeatherForecast/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RestSharp;
+using WeatherForecast.Models;
+
+namespace WeatherForecast.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        string Baseurl = "https://www.metaweather.com/";
+        public async Task<ActionResult> Index()
+        {
+            List<Weather> weathers = new List<Weather>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage message = await client.GetAsync("api/location/44418/2013/4/27/");
+
+                if (message.IsSuccessStatusCode)
+                {
+                    var WeatherResponse = message.Content.ReadAsStringAsync().Result;
+                    weathers = JsonConvert.DeserializeObject<List<Weather>>(WeatherResponse);
+                }
+            }
+            return View(weathers);
+        }
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WeatherForecast/Models/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.Models
+{
+    public class Weather
+    {
+        public DateTime applicable_date { get; set; }
+        public string weather_state_name { get; set; }
+        public string Image { get; set; }
+    }
+   
+}
